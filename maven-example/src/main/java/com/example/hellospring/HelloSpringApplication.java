adc70119fd52acf24d278036c788696cc5c704f4
XDD Tech <xddtech@users.noreply.github.com>
2025-03-26 18:48:32 -0400

    Update HelloSpringApplication.java
    xdd update 2

--- maven-example/src/main/java/com/example/hellospring/HelloSpringApplication.java
+++ maven-example/src/main/java/com/example/hellospring/HelloSpringApplication.java
@@ -39,7 +39,7 @@ public class HelloSpringApplication implements CommandLineRunner {
 	}
 
 	public final static String getBanner(String mode) {
-		final String greeting = "Hello from Google Cloud! XDD";
+		final String greeting = "Hello from Google Cloud! XDD 1";
 
 		final String logoBW = 
 		"                                                                                \n"+
